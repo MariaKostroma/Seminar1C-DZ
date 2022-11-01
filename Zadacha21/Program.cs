42cb941db677dfa4bcbf3a89d071b553478bde77
Maria <kostroma-mn@yandex.ru>
2022-11-01 15:11:27 +0300

    Zadacha 21 reshena

---
+++ Zadacha21/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача21. Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты двух точек");
+double num1X = double.Parse(Console.ReadLine());
+double num1Y = double.Parse(Console.ReadLine());
+double num1Z = double.Parse(Console.ReadLine());
+double num2X = double.Parse(Console.ReadLine());
+double num2Y = double.Parse(Console.ReadLine());
+double num2Z = double.Parse(Console.ReadLine());
+
+double distination = Math.Sqrt(Math.Pow((num1X-num2X),2)+Math.Pow((num1Y-num2Y),2)+Math.Pow((num1Z-num2Z),2));
+
+Console.WriteLine($"Рассояние между точками: {distination}");
