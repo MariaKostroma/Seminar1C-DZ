63d5334bf6a53cdfa0cdecc8080bf421788bbc34
Maria <kostroma-mn@yandex.ru>
2022-11-01 17:51:22 +0300

    Zadacha 21 reshena1

--- Zadacha21/Program.cs
+++ Zadacha21/Program.cs
@@ -5,10 +5,11 @@ Console.WriteLine("Введите координаты двух точек");
 double num1X = double.Parse(Console.ReadLine());
 double num1Y = double.Parse(Console.ReadLine());
 double num1Z = double.Parse(Console.ReadLine());
+
 double num2X = double.Parse(Console.ReadLine());
 double num2Y = double.Parse(Console.ReadLine());
 double num2Z = double.Parse(Console.ReadLine());
 
-double distination = Math.Sqrt(Math.Pow((num1X-num2X),2)+Math.Pow((num1Y-num2Y),2)+Math.Pow((num1Z-num2Z),2));
+double distination = Math.Sqrt(Math.Pow((num2X-num1X),2)+Math.Pow((num2Y-num1Y),2)+Math.Pow((num2Z-num1Z),2));
 
 Console.WriteLine($"Рассояние между точками: {distination}");
