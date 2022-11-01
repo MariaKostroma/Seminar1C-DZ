cc0161b6d0566ec7b47be7e0b8b7f6c46624b84b
Maria <kostroma-mn@yandex.ru>
2022-11-01 13:59:14 +0300

    Zadacha 19 reshena

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача19. Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<10000)
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+else
+{
+    int firstnum = num / 10000;
+    int fifthnum = num % 10;
+    int secondnum = (num / 1000) % 10;
+    int forthnum = (num / 10) % 10;
+    if (firstnum == fifthnum && secondnum == forthnum)
+    {
+      Console.WriteLine("Число является палиндромом");  
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
