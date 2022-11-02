c34114cf388d463200c2fd7de23c48431808b2e0
Maria <kostroma-mn@yandex.ru>
2022-11-02 10:28:03 +0300

    Zadacha 23 reshena

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+//кубов чисел от 1 до N.
+
+int num = SetNumber("N");
+
+int SetNumber(string str)
+{
+    System.Console.WriteLine($"Введите число {str}");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+    for (int i = 1; i<= num; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.WriteLine();
+
